4367fc2f21fcb0c8407ec73a9c2a2f261f5e7abb
wj1089 <wj1089@naver.com>
2020-07-06 19:34:41 +0900

    20.07.06
    19:33
    *리액이 화면띄우기
    * 챗봇화면 이동

---
+++ src/components/ChatBot.tsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+class ChatBot extends Component{
+    render() {
+        return (
+            <div>
+                <h1> 챗봇 입니다.</h1>
+            </div>
+        );
+    }
+}
+export default ChatBot;
