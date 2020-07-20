427e818ff680e765cc48f5c21512779dcdf80f14
wj1089 <wj1089@naver.com>
2020-07-20 18:26:29 +0900

    20.07.20

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux'
 import * as serviceWorker from './serviceWorker';
 import App from './App';
-
+import store from "./utiles/store"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App/>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
